3cc83378f22c6f84a8faa6e686e890965e82459e
SerenaSinha31 <114794148+SerenaSinha31@users.noreply.github.com>
2022-10-15 16:18:05 +0530

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,26 @@
+
+
+
+My_first_name = "Serena"
+print(My_first_name)
+x, y, z = 10, 20, 30
+print(x)
+print(y)
+print(z)
+x = y = z = 10
+print(x,y,z)
+x="my name is serena"
+print(x)
+x="my"
+y="name"
+z="is"
+k="serena"
+print(x,y,z,k)
+print(x+y+z+k)
+x= 10
+y= "hello"
+print(x,y)
+#print(x+y)#string and integer cannot be added
+
+
+
