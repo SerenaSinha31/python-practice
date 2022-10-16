9ee9b5f7bc1b90a82ff0f242d611d1beca5d170d
SerenaSinha31 <114794148+SerenaSinha31@users.noreply.github.com>
2022-10-16 13:50:34 +0530

    Add files via upload

---
+++ prog4string.py
@@ -0,0 +1,33 @@
+x = "Hi everyone"
+print(x.upper())
+#in uppercase
+print(x.lower())
+#in lowezrcase
+
+#remove whitespaces
+x1 = "   let's study today"
+print(x1.strip())
+
+#replace
+x2 = "let us play together"
+print(x2.replace("g","q"))
+
+#splitting a string
+T = "Hello World"
+print(T.split())
+
+#string concatenation
+A = "Hello Everyone"
+B =  A.split()
+print(B[1])
+
+W = "toadyisa"
+E = "coldday"
+print(W+E)
+
+t = "hello" 
+q = "everyone"
+U = t+" "+q
+print(U)
+
+
